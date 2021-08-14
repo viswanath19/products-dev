8ca5aa0a3cfe1ed99d77e4922c22bbff64ffe394
ViswanathSarma <viswanath.sarma@acquire.io>
2021-08-14 11:27:13 +0530

    basic view setup

---
+++ src/components/ProductView.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProductView extends Component {
+    render() {
+        return (
+            <div>
+                This is the products view
+            </div>
+        )
+    }
+}
+
+export default ProductView
